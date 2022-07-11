2febd81099d9a9a49710e8324e98e2d46465d3c6
Thomas R <109040055+Thomas-Roussel@users.noreply.github.com>
2022-07-11 14:47:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const fetch = (...args) =>
 
 module.exports = {
 
-    getServer: async function (apikey, serverId) {
+    getServer: async function (apikey, serverId, inSeconds) {
 
         if (!apikey || apikey == '') throw new Error('No API Key sended')
         if (!serverId || serverId == '') throw new Error('No server id sended')
 
-        let reponse = await fetch(`https://api.friends-bot.fr/servers/${apikey}/${serverId}`, { method: 'GET' })
+        var reponse;
+        if(inSeconds === true) reponse = await fetch(`https://api.friends-bot.fr/servers/${apikey}/${serverId}?time=seconds`, { method: 'GET' })
+        else reponse = await fetch(`https://api.friends-bot.fr/servers/${apikey}/${serverId}`, { method: 'GET' })
 
         if(reponse == 'Invalid API Key') throw new Error('Invalid API Key sended')
         if(reponse == 'Invalid server id') throw new Error('Invalid server id sended')
